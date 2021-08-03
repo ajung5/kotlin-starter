12f0f2f7f0d83d7777f8e277a8a452b2c07c5752
Ajung <agungnawawi@gmail.com>
2021-08-03 10:44:31 +0700

    Commit Kedelapan

--- src/main/kotlin/com/iseng/kotlin/example1.kt
+++ src/main/kotlin/com/iseng/kotlin/example1.kt
@@ -1,9 +1,55 @@
 package com.iseng.kotlin
 
 fun main () {
-    for (i in 1..0) {
-        val  prefix = if (i !in 4..7) "-" else ">"
-        println("$prefix $i")
-    }
+    /*
+    val rentang = 1..100
+    println (rentang.count())
 
+    for (j in 1..10) {
+        println(j)
+    }
+    */
+    for (j in 1..15) {
+        // if (j !in 4..7 ) print("-") else print(">")
+        /*
+        val prefix = if (j !in 4..7) "*" else ">"
+        println("$prefix $j")
+        */
+        when (j) {
+            in 4..8 -> {
+                print("*")
+            }
+            in 12..13 -> {
+                print(">")
+            }
+            else -> {
+                print("-")
+            }
+        }
+        println(j)
+        /*
+        val prefix: Unit = when (j) {
+            in 4..8 -> {
+                print("*")
+            }
+            !in 12..13 -> {
+                print(">")
+            }
+            else -> {
+                print("-")
+            }
+        }
+        println("$prefix $j")
+        */
+        /*
+        val prefix: Unit = if (j in 4..8 ) {
+            print("*")
+        }else if (j !in 12..13) {
+            print(">")
+        } else {
+            print("-")
+        }
+        println("$prefix $j")
+        */
+    }
 }
