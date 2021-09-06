11c1fcd1832673b27e60ca04c3d38fe77f5e457c
Ajung <agungnawawi@gmail.com>
2021-09-06 13:58:25 +0700

    Commit nambah aja

--- src/main/kotlin/com/dicoding/kotlin/28_ FunctionInfixNotation.kt
+++ src/main/kotlin/com/dicoding/kotlin/28_ FunctionInfixNotation.kt
@@ -1,6 +1,15 @@
 package com.dicoding.kotlin
 
+infix fun String.to(type: String): String {
+    if (type == "UP") {
+        return this.toUpperCase()
+    } else {
+        return this.toLowerCase()
+    }
+}
+
+
 fun main() {
- val nama = "aa"
-    val = "nambah aja"
+    val result = "Agung Nawawi" to "UP"
+    println(result)
 }
---
+++ src/main/kotlin/com/dicoding/kotlin/29_functionScope.kt
@@ -0,0 +1,5 @@
+package com.dicoding.kotlin
+
+//function yang hanya bisa diakses didalam function main. ga bisa diakses diluar function main
+
+fun main() {}
